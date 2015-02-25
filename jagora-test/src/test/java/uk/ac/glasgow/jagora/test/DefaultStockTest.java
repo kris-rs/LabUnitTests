83f6b69dea1d350a94cfbc1ba1d3ed36ac9cf06e
Chinook- <rammluft@gmail.com>
2015-02-25 22:02:15 +0000

    Added Stock test

---
+++ jagora-test/src/test/java/uk/ac/glasgow/jagora/test/DefaultStockTest.java
@@ -0,0 +1,30 @@
+package uk.ac.glasgow.jagora.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.ac.glasgow.jagora.Stock;
+import uk.ac.glasgow.jagora.impl.DefaultStock;
+
+public class DefaultStockTest {
+
+	Stock apples;
+	
+	@Before
+	public void setUp(){
+		apples = new DefaultStock("apples");
+	}
+	
+	@After
+	public void tearDown(){
+		apples = null;
+	}
+	
+	@Test
+	public void getNameTest(){
+		assertTrue(apples.getName().equals("apples"));
+	}
+}
